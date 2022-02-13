f608fca3f6d82e61a8549bf28d48380e190872bb
Ken Powers <ken@kenpowers.net>
2022-02-12 20:45:00 -0600

    Implement crates search and multi-search

--- app/utils/search.test.ts
+++ app/utils/search.test.ts
@@ -1,33 +1,165 @@
-import { NpmResponseJson, searchNpm } from './search';
+import { isString } from 'lodash';
+import search, {
+  CratesResponseJson,
+  NpmResponseJson,
+  ResponseJson,
+  searchCrates,
+  searchNpm,
+} from './search';
 
-function spyOnFetch<T>(body: T) {
-  jest.spyOn(global, 'fetch').mockResolvedValue(
-    new Response(JSON.stringify(body), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }),
-  );
+function spyOnFetch<T>(
+  responses: Record<string, { status?: number; contentType?: string; body: T }>,
+) {
+  jest.spyOn(global, 'fetch').mockImplementation((...args) => {
+    const url = args[0];
+
+    if (!isString(url)) {
+      return Promise.reject();
+    }
+
+    for (const [
+      key,
+      { status = 200, contentType = 'application/json', body },
+    ] of Object.entries(responses)) {
+      if (url.includes(key)) {
+        return Promise.resolve(
+          new Response(JSON.stringify(body), {
+            status,
+            headers: { 'Content-Type': contentType },
+          }),
+        );
+      }
+    }
+
+    return Promise.reject();
+  });
 }
 
 describe('search', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('npm', () => {
     test('searchNpm', async () => {
       spyOnFetch<NpmResponseJson>({
-        objects: [
-          {
-            package: {
-              date: '2021-02-20T15:42:16.891Z',
-              links: { repository: 'https://github.com/lodash/lodash' },
-            },
+        npmjs: {
+          body: {
+            objects: [
+              {
+                package: {
+                  date: '2021-02-20T15:42:16.891Z',
+                  links: { repository: 'https://github.com/lodash/lodash' },
+                },
+              },
+            ],
           },
-        ],
+        },
       });
-      expect(await searchNpm('test')).toEqual({
-        timestamp: new Date('2021-02-20T15:42:16.891Z').getTime(),
+      expect(await searchNpm('lodash')).toEqual({
+        timestamp: 1613835736891,
         repo: 'https://github.com/lodash/lodash',
       });
+      expect(global.fetch).toBeCalledTimes(1);
+      expect(global.fetch).toBeCalledWith(
+        'https://registry.npmjs.com/-/v1/search?text=lodash&size=1',
+      );
+    });
+  });
+
+  describe('crates', () => {
+    test('searchCrates', async () => {
+      spyOnFetch<CratesResponseJson>({
+        crates: {
+          body: {
+            crates: [
+              {
+                updated_at: '2021-06-06T11:00:31.172403+00:00',
+                repository: 'https://github.com/BurntSushi/fst',
+              },
+            ],
+          },
+        },
+      });
+      expect(await searchCrates('fst')).toEqual({
+        timestamp: 1622977231172,
+        repo: 'https://github.com/BurntSushi/fst',
+      });
+      expect(global.fetch).toBeCalledTimes(1);
+    });
+  });
+
+  describe('all together', () => {
+    test('npm is newest', async () => {
+      spyOnFetch<ResponseJson>({
+        npmjs: {
+          body: {
+            objects: [
+              {
+                package: {
+                  date: '2021-02-20T15:42:16.891Z',
+                  links: { repository: 'https://github.com/lodash/lodash' },
+                },
+              },
+            ],
+          },
+        },
+        crates: {
+          body: {
+            crates: [
+              {
+                updated_at: '2016-12-25T14:35:55.101357+00:00',
+                repository: 'meh',
+              },
+            ],
+          },
+        },
+      });
+
+      const res = await search('lodash');
+
+      expect(res).toEqual({
+        repo: 'https://github.com/lodash/lodash',
+        timestamp: 1613835736891,
+      });
+
+      expect(global.fetch).toBeCalledTimes(2);
+    });
+
+    test('crates is newest', async () => {
+      spyOnFetch<ResponseJson>({
+        npmjs: {
+          body: {
+            objects: [
+              {
+                package: {
+                  date: '2017-02-07T03:58:00.320Z',
+                  links: { repository: 'https://github.com/dropbox/zxcvbn' },
+                },
+              },
+            ],
+          },
+        },
+        crates: {
+          body: {
+            crates: [
+              {
+                updated_at: '2021-10-11T19:53:47.159715+00:00',
+                repository: 'https://github.com/shssoichiro/zxcvbn-rs',
+              },
+            ],
+          },
+        },
+      });
+
+      const res = await search('zxcvbn');
+
+      expect(res).toEqual({
+        repo: 'https://github.com/shssoichiro/zxcvbn-rs',
+        timestamp: 1633982027159,
+      });
+
+      expect(global.fetch).toBeCalledTimes(2);
     });
   });
 });
--- app/utils/search.ts
+++ app/utils/search.ts
@@ -1,12 +1,24 @@
+import { maxBy } from 'lodash';
 import urlcat from 'urlcat';
-
-export interface NpmResponseJson {
-  objects: Array<{ package: { date: string; links: { repository: string } } }>;
-}
 
 export interface SearchResult {
   repo: string;
   timestamp: number;
+}
+
+async function fetchJson<T>(url: string): Promise<T> {
+  const res = await fetch(url);
+  const data = (await res.json()) as T;
+
+  return data;
+}
+
+function dateStringToMs(date: string) {
+  return new Date(date).getTime();
+}
+
+export interface NpmResponseJson {
+  objects: Array<{ package: { date: string; links: { repository?: string } } }>;
 }
 
 export async function searchNpm(text: string): Promise<SearchResult | null> {
@@ -15,18 +27,44 @@ export async function searchNpm(text: string): Promise<SearchResult | null> {
     size: 1,
   });
 
-  const res = await fetch(url);
-  const data = (await res.json()) as NpmResponseJson;
+  const data = await fetchJson<NpmResponseJson>(url);
   const pack = data.objects[0]?.package;
 
-  return pack
+  return pack && pack.links.repository
     ? {
         repo: pack.links.repository,
-        timestamp: new Date(pack.date).getTime(),
+        timestamp: dateStringToMs(pack.date),
       }
     : null;
 }
 
+export interface CratesResponseJson {
+  crates: Array<{ updated_at: string; repository: string }>;
+}
+
+export async function searchCrates(q: string): Promise<SearchResult | null> {
+  const url = urlcat(
+    'https://crates.io/api/v1/crates?page=1&per_page=1&q=fst',
+    { page: 1, per_page: 1, q: q },
+  );
+
+  const data = await fetchJson<CratesResponseJson>(url);
+  const crate = data.crates[0];
+
+  return crate
+    ? {
+        repo: crate.repository,
+        timestamp: dateStringToMs(crate.updated_at),
+      }
+    : null;
+}
+
+export type ResponseJson = NpmResponseJson | CratesResponseJson;
+
 export default async function search(text: string) {
-  return searchNpm(text);
+  const allRes = (
+    await Promise.all([searchNpm(text), searchCrates(text)])
+  ).filter((el): el is SearchResult => !!el);
+
+  return maxBy(allRes, 'timestamp');
 }
