39b3cd09b8b2990534f040a89676062771162004
Ken Powers <ken@kenpowers.net>
2022-02-18 21:26:46 -0600

    Add short url reporting to plausible

--- app/routes/$.ts
+++ app/routes/$.ts
@@ -2,7 +2,47 @@ import { LoaderFunction, redirect } from 'remix';
 import parse from '~/utils/parse';
 import search from '~/utils/search';
 
-export const loader: LoaderFunction = async ({ params, request }) => {
+const plausibleEndpoint = 'https://plausible.io/api/event';
+
+function reportToPlausible(
+  text: string,
+  scope: string,
+  name: string,
+  mode: string,
+  referrer: string,
+  userAgent: string,
+) {
+  const props = { scope, name, mode };
+  const baseBody = {
+    domain: 'git.pizza',
+    url: `https://git.pizza/${text}`,
+    referrer,
+  };
+  const baseOps = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': userAgent,
+    },
+  };
+  fetch(plausibleEndpoint, {
+    ...baseOps,
+    body: JSON.stringify({
+      ...baseBody,
+      name: 'pageView',
+    }),
+  });
+  fetch(plausibleEndpoint, {
+    ...baseOps,
+    body: JSON.stringify({
+      ...baseBody,
+      name: 'short url',
+      props,
+    }),
+  });
+}
+
+export const loader: LoaderFunction = async ({ params, request, ...rest }) => {
   const cookieHeader = request.headers.get('Cookie') ?? '';
   const modeCookieValue = decodeURIComponent(
     /mode=(.+?)(?:;|$)/.exec(cookieHeader)?.[1] ?? '',
@@ -13,17 +53,23 @@ export const loader: LoaderFunction = async ({ params, request }) => {
 
   const text = params['*'] ?? '';
   const { scope, name } = parse(text);
+  const mode = modeCookieValue || 'newest';
+
+  reportToPlausible(
+    text,
+    scope || '',
+    name || '',
+    mode,
+    request.referrer,
+    request.headers.get('User-Agent') || 'unknown',
+  );
 
   if (!name) {
     return redirect('/');
   }
 
   const { repo = '/' } =
-    (await search(
-      name,
-      scope || scopesCookieValue,
-      modeCookieValue || 'newest',
-    )) ?? {};
+    (await search(name, scope || scopesCookieValue, mode)) ?? {};
 
   return redirect(repo);
 };
